015bfdba6bd93264449b288d02f51fcbfaef3ce0
Brendan Quinn <brendan-quinn@northwestern.edu>
2021-01-16 16:53:16 -0600

    Parse S3.getObject buffer instead of tempfile

--- index.js
+++ index.js
@@ -1,50 +1,32 @@
 const AWS = require("aws-sdk");
-const URI = require("uri-js");
-const fs = require("fs");
-const tempy = require("tempy");
 const exifr = require("exifr");
 
 AWS.config.s3 = {
+  accessKeyId: "minio",
+  secretAccessKey: "minio123",
   endpoint: "https://devbox.library.northwestern.edu:9001",
   s3ForcePathStyle: true,
 };
 
-const parseExif = async (bucket, key) => {
-  const inputFile = await makeInputFile(bucket, key);
-  try {
-    let output = await exifr
-      .parse(inputFile, { chunked: false })
-      .then((output) => console.log(output));
-    return output;
-  } finally {
-    fs.unlink(inputFile, (err) => {
-      if (err) {
-        throw err;
+const downloadImage = (bucket, key) => {
+  new AWS.S3().getObject(
+    {
+      Bucket: bucket,
+      Key: key,
+    },
+    function (error, data) {
+      if (error != null) {
+        console.log(error);
+      } else {
+        let exif_data = exifr
+          .parse(data.Body)
+          .catch((error) => console.log(error));
+        return exif_data.then(function (result) {
+          console.log(result);
+        });
       }
-    });
-  }
+    }
+  );
 };
 
-const makeInputFile = (bucket, key) => {
-  return new Promise((resolve, reject) => {
-    let fileName = tempy.file();
-    let writable = fs
-      .createWriteStream(fileName)
-      .on("error", (err) => reject(err));
-    let s3Stream = new AWS.S3()
-      .getObject({
-        Bucket: bucket,
-        Key: key,
-      })
-      .createReadStream();
-
-    s3Stream.on("error", (err) => reject(err));
-
-    s3Stream
-      .pipe(writable)
-      .on("close", () => resolve(fileName))
-      .on("error", (err) => reject(err));
-  });
-};
-
-// parseExif("dev-ingest", "opo0328a.tif");
+downloadImage("dev-ingest", "inu-dil-15de9e70-f6cc-4bd2-ba74-3dabab92b1d8.tif");
